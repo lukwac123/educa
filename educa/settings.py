6c933359ad4a14f1a6fc6e263b5fc6e61393b012
lukwac123 <lukasz-waclawek@wp.pl>
2024-12-27 12:42:54 +0100

    Dodanie obsługi multimedialnej plików.

--- educa/settings.py
+++ educa/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -31,6 +33,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Moje aplikacje
+    'courses.apps.CoursesConfig',
+    # Pozostałe aplikacje
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
