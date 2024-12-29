27b5acf6b27c1ffc6a09dfcea8b2bcecbeac3359
lukwac123 <lukasz-waclawek@wp.pl>
2024-12-29 22:41:10 +0100

    Widok zmiany kolejności modułów kursu.

--- courses/views.py
+++ courses/views.py
@@ -9,6 +9,7 @@ from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps
 from .models import Module, Content
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 class OwnerMixin:
     def get_queryset(self):
@@ -121,3 +122,10 @@ class ModuleContentListView(TemplateResponseMixin, View):
     def get(self, request, module_id):
         module = get_object_or_404(Module, id=module_id, course__owner=request.user)
         return self.render_to_response({'module': module})
+    
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id, course_owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'ok'})
+    
