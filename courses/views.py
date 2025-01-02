145343a8fd8d348acbf8401e30934766ef767fe6
lukwac123 <lukasz-waclawek@wp.pl>
2025-01-02 22:43:10 +0100

    Widok wszstkich tematów oraz całkowita liczba kursów.

--- courses/views.py
+++ courses/views.py
@@ -10,6 +10,8 @@ from django.forms.models import modelform_factory
 from django.apps import apps
 from .models import Module, Content
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+from django.db.models import Count
+from .models import Subject
 
 class OwnerMixin:
     def get_queryset(self):
@@ -134,4 +136,16 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
         for id, order in self.request_json.items():
             Content.objects,filter(id=id, module__course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'ok'})
+    
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects, 'subject': subject, 'courses': courses})
     
