51676dfa15d1d62b25b0b803bbc122150461b012
lukwac123 <lukasz-waclawek@wp.pl>
2025-02-01 18:04:04 +0100

    Szablon listy kursów użytkownika oraz dostęp do treści dla zapisanych kursów.

--- educa/settings.py
+++ educa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,4 +129,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL='/course/mine/'
+LOGIN_REDIRECT_URL= reverse_lazy('student_course_list')
