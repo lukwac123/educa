7901667b2e09d7654ead4d87ab98eae5011e192c
lukwac123 <lukasz-waclawek@wp.pl>
2025-02-02 16:03:13 +0100

    Instalacja frameworka REST Django.

--- educa/settings.py
+++ educa/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'embed_video',
     'debug_toolbar',
     'redisboard',
+    'rest_framework',
     # Pozostałe aplikacje
     'django.contrib.admin',
     'django.contrib.auth',
@@ -151,3 +152,9 @@ INTERNAL_IPS = [
 CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = 60 * 15 # 15 minut
 CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
