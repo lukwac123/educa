571b1e0aa5bbdfd8a0d010cd59eb0807bd9c122d
lukwac123 <lukasz-waclawek@wp.pl>
2025-02-02 17:50:57 +0100

    Uwierzytelnianie i określanie uprawnień do widoków.

--- courses/api/views.py
+++ courses/api/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import generics
 from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
@@ -14,6 +16,9 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
