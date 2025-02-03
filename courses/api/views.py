689cfccbeab967629dc9e13dc821d8c3119b61bf
lukwac123 <lukasz-waclawek@wp.pl>
2025-02-03 18:43:37 +0100

    Tworzenie niestandardowych uprawnień i serializacja treści kursu.

--- courses/api/views.py
+++ courses/api/views.py
@@ -8,6 +8,8 @@ from courses.api.serializers import SubjectSerializer, CourseSerializer
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
+from courses.api.permissions import IsEnrolled
+from courses.api.serializers import CourseWithContentsSerializer
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
@@ -19,6 +21,12 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+    
+    @action(detail=True, methods=['get'], serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
