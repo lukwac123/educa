b95d01e01f51f7712f9238f2ec4aa616674ad25c
lukwac123 <lukasz-waclawek@wp.pl>
2025-02-03 19:17:43 +0100

    Wykorzystanie API do zewnętrznego zapisania użytkownika na kursy.

--- api_examples/enroll_all.py
+++ api_examples/enroll_all.py
@@ -1,5 +1,7 @@
 import requests
 
+username = '' # Odpowiada odpowiednim poświadczeniom istniejącego użytkownika
+password = '' # Odpowiada odpowiednim poświadczeniom istniejącego użytkownika
 base_url = 'http://127.0.0.1:8000/api/'
 
 # Pobranie wszystkich kursów
@@ -8,3 +10,12 @@ courses = r.json()
 
 available_courses = ', '.join([course['title'] for course in courses])
 print(f'Dostępne kursy: {available_courses}')
+
+for course in courses:
+    course_id = course['id']
+    course_title = course['title']
+    r = requests.post(f'{base_url}courses/{course_id}/enroll/', auth=(username, password))
+
+    if r.status_code == 200:
+        # Żądanie zakończone sukcesem
+        print(f'Pomyślnie zapisałeś się na kurs {course_title}')
